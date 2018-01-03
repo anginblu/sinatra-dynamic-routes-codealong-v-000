a8b37de2de739d0beba0526d6fff48368c0ed439
Jacqueline Lee <anginblu@gmail.com>
2018-01-03 23:41:18 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -22,6 +22,7 @@ class App < Sinatra::Base
   post '/multiply/:number1/:number2' do
     @number1 = params[:number1].to_i
     @number2 = params[:number2].to_i
+    @number3 = @number1 * @number2
     "#{@number1*@number2}"
   end
 
